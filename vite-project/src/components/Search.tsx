30739f52b75f2a5cd0862cff0ba146b2debc8ec3
@Nickbis <nickbis3@gmail.com>
2023-11-04 21:36:05 +0300

    refactot: convert to function search()

--- vite-project/src/components/Search.tsx
+++ vite-project/src/components/Search.tsx
@@ -1,68 +1,47 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Props {
   onQuery: (query: string) => void;
 }
 
-interface State {
-  query: string;
-}
-
 export const LocalStorageKey = 'lastSearch';
 
-export default class Search extends Component<Props, State> {
-  state: State = {
-    query: '',
+export default function Search(props: Props) {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
   };
 
-  constructor(props: Props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({
-      query: event.target.value,
-    });
-  }
-
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const { query } = this.state;
+
     if (query) {
-      this.props.onQuery(query);
+      props.onQuery(query);
       localStorage.setItem(LocalStorageKey, query);
     } else {
-      this.props.onQuery(query);
+      props.onQuery(query);
       localStorage.setItem(LocalStorageKey, '');
-      this.setState({
-        query: '',
-      });
+      setQuery('');
     }
-  }
+  };
 
-  componentDidMount() {
+  useEffect(() => {
     const lastSearch = localStorage.getItem(LocalStorageKey);
     if (lastSearch) {
-      this.setState({
-        query: lastSearch,
-      });
+      setQuery(lastSearch);
     }
-  }
+  }, []);
 
-  render() {
-    const { query } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={query}
-          onChange={this.handleChange}
-          placeholder="Enter your request"
-        />
-        <button type="submit">Search</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Enter your request"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 }
