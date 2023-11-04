a57deed7c42aff2b5cfc36ee538607e524ca97fc
@Nickbis <nickbis3@gmail.com>
2023-11-04 23:22:39 +0300

    refactor: convert to function result()

--- vite-project/src/components/Results.tsx
+++ vite-project/src/components/Results.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ApiSearchResult } from './types';
 import { OnSearchApi } from './api';
 import { LocalStorageKey } from './Search';
@@ -6,63 +6,56 @@ import { LocalStorageKey } from './Search';
 interface Props {
   query: string;
 }
-interface State {
-  results: ApiSearchResult[];
-}
-export default class Results extends React.Component<Props, State> {
-  state: State = {
-    results: [],
-  };
-  API: OnSearchApi;
-  constructor(props: Props) {
-    super(props);
-    this.API = OnSearchApi.getInstance();
-    this.API.getSearch = this.API.getSearch.bind(this);
-  }
 
-  async componentDidMount() {
-    const lastSearch = localStorage.getItem(LocalStorageKey);
-    const results: ApiSearchResult[] = await this.API.getSearch(lastSearch);
-    this.setState({ results });
-  }
+export default function Results(props: Props) {
+  const [results, setResults] = useState<ApiSearchResult[]>([]);
 
-  async componentDidUpdate(prevProps: Readonly<Props>) {
-    if (prevProps.query !== this.props.query) {
-      const { query } = this.props;
-      const results: ApiSearchResult[] = await this.API.getSearch(query);
-      this.setState({ results });
-    }
-  }
+  const API = OnSearchApi.getInstance();
 
-  render() {
-    const { results } = this.state;
-    if (results) {
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Image URL</th>
+  useEffect(() => {
+    const fetchResults = async () => {
+      const lastSearch = localStorage.getItem(LocalStorageKey);
+      const results: ApiSearchResult[] = await API.getSearch(lastSearch);
+      setResults(results);
+    };
+    fetchResults();
+  }, []); // eslint-disable-line
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const { query } = props;
+      const results: ApiSearchResult[] = await API.getSearch(query);
+      setResults(results);
+    };
+    fetchResults();
+  }, [props.query]); // eslint-disable-line
+
+  if (results) {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Image URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((results) => (
+            <tr key={results.id}>
+              <td>{results.id}</td>
+              <td>{results.name}</td>
+              <td>{results.gender}</td>
+              <td>
+                <a href={results.image}>{results.image}</a>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {results.map((results) => (
-              <tr key={results.id}>
-                <td>{results.id}</td>
-                <td>{results.name}</td>
-                <td>{results.gender}</td>
-                <td>
-                  <a href={results.image}>{results.image}</a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      );
-    } else {
-      return <h3>Not found</h3>;
-    }
+          ))}
+        </tbody>
+      </table>
+    );
+  } else {
+    return <h3>Not found</h3>;
   }
 }
